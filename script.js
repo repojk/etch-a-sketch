5ee59b07bffaf6f64c16a223f060e5cbc4e62951
repojk <gitofjk@gmail.com>
2022-09-21 22:17:09 -0700

    Dynamically size gridSqaures

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let gridSize = 16;
+let gridSide = 16;
 let borderSize = 3; //default border size is 3
-let gridContainerSide = ((gridSize * 20) + (borderSize * gridSize * 2)).toString() + 'px';
+let gridSquareSize = 30;
+let gridContainerSide = ((gridSide * gridSquareSize) + (borderSize * gridSide * 2)).toString() + 'px';
 
 const divGridContainer = document.querySelector('#grid-container');
 divGridContainer.classList.add('grid-container', 'wrap');
 divGridContainer.setAttribute('style', `width: ${gridContainerSide}; height: ${gridContainerSide};`);
 
-for (i = 0; i < (gridSize * gridSize); i++) {
+for (i = 0; i < (gridSide * gridSide); i++) {
     const divGridSquare = document.createElement('div');
     divGridSquare.classList.toggle('grid-square');
+    divGridSquare.setAttribute('style', `width: ${gridSquareSize}px; height: ${gridSquareSize}px;`);
     divGridContainer.appendChild(divGridSquare);
 }
