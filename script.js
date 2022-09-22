6bb37a2a0d974a56f4e80842a8b3221defb3c5c8
repojk <gitofjk@gmail.com>
2022-09-22 16:40:19 -0700

    Implement trail when mouse passes through grid

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let gridSide = 16;
-let borderSize = 3; //default border size is 3
-let gridSquareSize = 30;
-let gridContainerSide = ((gridSide * gridSquareSize) + (borderSize * gridSide * 2)).toString() + 'px';
+function initializeGrid() {
+    let gridSide = 16;
+    //set borderSize to 3 if checking .grid-square class displaying border
+    let borderSize = 0;
+    let gridSquareSize = 30;
+    let gridContainerSide = ((gridSide * gridSquareSize) + (borderSize * gridSide * 2)).toString() + 'px';
 
-const divGridContainer = document.querySelector('#grid-container');
-divGridContainer.classList.add('grid-container', 'wrap');
-divGridContainer.setAttribute('style', `width: ${gridContainerSide}; height: ${gridContainerSide};`);
+    const divGridContainer = document.querySelector('#grid-container');
+    divGridContainer.classList.add('grid-container', 'wrap');
+    divGridContainer.setAttribute('style', `width: ${gridContainerSide}; height: ${gridContainerSide};`);
 
-for (i = 0; i < (gridSide * gridSide); i++) {
-    const divGridSquare = document.createElement('div');
-    divGridSquare.classList.toggle('grid-square');
-    divGridSquare.setAttribute('style', `width: ${gridSquareSize}px; height: ${gridSquareSize}px;`);
-    divGridContainer.appendChild(divGridSquare);
+    for (i = 0; i < (gridSide * gridSide); i++) {
+        const divGridSquare = document.createElement('div');
+        divGridSquare.classList.toggle('grid-square');
+        divGridSquare.setAttribute('style', `width: ${gridSquareSize}px; height: ${gridSquareSize}px;`);
+        divGridContainer.appendChild(divGridSquare);
+    }
+};
+
+function hoverTrail(e) {
+    this.classList.add('trail');
+};
+
+function listenHoverTrail() {
+    const hoverGridSquares = document.querySelectorAll('.grid-square');
+    hoverGridSquares.forEach(square => square.addEventListener('mouseover', hoverTrail));
+};
+
+function main() {
+    initializeGrid();
+    listenHoverTrail();
 }
+
+main();
